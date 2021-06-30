6bc5467a2fc4241adb7418e4fd50f36c8ab74d29
tylerakin2348 <tylerakinmwc@gmail.com>
2021-06-29 22:48:28 -0400

    d

--- routes/acting-credits.js
+++ routes/acting-credits.js
@@ -10,6 +10,7 @@ router.get("/", async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 });
+
 router.get("/:id", getActingCredit, (req, res) => {
   res.send(res.acting_credit);
 });
