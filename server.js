0cb7ee0972848617f9fa8582a7baeab61cd21be9
tylerakin2348 <tylerakinmwc@gmail.com>
2021-07-03 09:28:50 -0400

    updates server port to avoid crossing wires

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 const ACTOR_API_VERSION = process.env.ACTOR_API_VERSION || "v1";
 
 function determineDatabaseURL() {
